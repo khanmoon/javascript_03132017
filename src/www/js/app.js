2c734fa414ca448bfb6184724d0f1b1008948238
Eric W. Greene <eric@ericwgreene.com>
2017-03-14 15:34:55 -0400

    added code demos

--- src/www/js/app.js
+++ src/www/js/app.js
@@ -7,10 +7,22 @@ import $ from 'jquery';
 
 // $('li:last-child').remove();
 
+const profiles = [
+    { name: 'Jim' },
+    { name: 'Jim' }
+];
+
+$('li:last-child').prop('profileData', profiles[0]);
+
 $('ul').on('click', 'button', e => {
     console.log('I was clicked!');
     console.log('what I clicked on', e.target);
     console.log('element that handled the event', e.currentTarget);
+    
+    console.log($(e.target).closest('li').prop('profileData'));
+
+    profiles.indexOf($(e.target).closest('li').prop('profileData'));
+
     $(e.target).closest('li').remove();
 });
 
